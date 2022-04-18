aad2337903e896ebb9a2f8f588013e0d26d673df
unknown <theo.mezier@gmail.com>
2022-04-18 10:02:54 +0200

    Optimisation

--- src/bo/Magasin.java
+++ src/bo/Magasin.java
@@ -1,14 +1,14 @@
 package bo;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Magasin {
 	private List<Vehicule> listeVehicule = new ArrayList<>();
 	private List<Location> listeLocation = new ArrayList<Location>();
-	
+	static Scanner sc = new Scanner(System.in);
 	public List<Location> getListeLocation() {
 		return listeLocation;
 	}
@@ -17,7 +17,23 @@ public class Magasin {
 		this.listeLocation = listeLocation;
 	}
 
+	public void ajouterLocation(Location location) {
+		listeLocation.add(location);
+	}
 	
+	public void consulterLocationEnCours() {
+		List<Location> listeLocationEnCours = new ArrayList<>();
+		for(Location current : listeLocation) {
+			if(current.getDateFin() == null) {
+				listeLocationEnCours.add(current);
+			}
+		}
+		System.out.println("💸💸💸💸💸💸💸💸💸💸💸Location En Cours💸💸💸💸💸💸💸💸💸💸💸");
+		for(Location current : listeLocationEnCours) {
+			System.out.println("🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾🧾");
+			current.afficher();		
+		}
+	}
 	
 	public List<Vehicule> getListeVehicule() {
 		return listeVehicule;
@@ -60,6 +76,69 @@ public class Magasin {
 			}
 		}
 		return listeVehiculeTrouve;
+	}
+	
+	public void rechercheVehicule() {
+		
+		String recherche = "";
+		System.out.println("Veuillez rentrer le nom du modèle à rechercher :");
+		
+		recherche = sc.nextLine();
+		List<Vehicule> vRecherche = chercherVehicule(recherche);
+		afficherVehicule(vRecherche);
+	}
+	
+	public static void afficherVehicule(List<Vehicule> listeVehiculeTrouve) {
+
+		System.out.println("🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲Liste des Articles🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲");
+		for(int i = 0; i < listeVehiculeTrouve.size(); i++) {
+			System.out.println(i);
+			System.out.println(listeVehiculeTrouve.get(i).toString());
+		}
+	}
+	
+	public void creerArticle() {
+		int choix = -1;
+		
+		System.out.println("Voulez vous entrer :");
+		System.out.println("1 - Velo");
+		System.out.println("2 - Gyropode");
+		System.out.println("3 - Gyroroue");
+		choix = sc.nextInt();
+		System.out.println("Veuillez entrer la marque :");
+		sc.nextLine();
+		String marque = sc.nextLine();
+		System.out.println("Veuillez entrer le modèle :");
+		String modele = sc.nextLine();
+		LocalDate date = LocalDate.now();
+			switch(choix) {
+				case 1 :{
+					System.out.println("Veuillez entrer le nombre de vitesses");
+					int vitesse = sc.nextInt();
+					Velo velo = new Velo(marque,modele,date,vitesse);
+					ajouterVehicule(velo);
+					break;
+				}
+				case 2 :{
+					System.out.println("Veuillez entrer l'autonomie");
+					int autonomie = sc.nextInt();
+					System.out.println("Veuillez entrer la taille minimum");
+					double taillemin = sc.nextDouble();
+					Gyropode gyropode = new Gyropode(marque,modele,date,autonomie,taillemin);
+					ajouterVehicule(gyropode);
+					break;
+				}
+				case 3 :{
+					System.out.println("Veuillez entrer l'autonomie");
+					int autonomie = sc.nextInt();
+					Gyroroue gyroroue = new Gyroroue(marque,modele,date,autonomie);
+					ajouterVehicule(gyroroue);
+					break;
+				}default :{
+					System.out.println("Veuillez entrer une valeur correcte");
+				}
+			}
+		
 	}
 	
 
--- src/fr/system/Locaciel.java
+++ src/fr/system/Locaciel.java
@@ -10,6 +10,7 @@ import bo.Adresse;
 import bo.Client;
 import bo.Gyropode;
 import bo.Gyroroue;
+import bo.Location;
 import bo.Magasin;
 import bo.Vehicule;
 import bo.Velo;
@@ -17,6 +18,7 @@ import bo.Velo;
 public class Locaciel {
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
+		
 		Velo velo1 = new Velo("Yamaha", "Be-546", LocalDate.of(2019, 05, 20), 5);
 		Velo velo3 = new Velo("Yamaha", "Be-546", LocalDate.of(2018, 05, 20), 5);
 		Velo velo2 = new Velo("Yamaha", "V-45-y", LocalDate.of(2018, 07, 18), 7);
@@ -39,93 +41,54 @@ public class Locaciel {
 		Adresse ad02 = new Adresse(7, "rue", "Augustin Truveau", "85000", "Paillasson les Bains");
 		Adresse ad03 = new Adresse(6, "avenue", "Guillaume Tell", "83200", "Bonlieu");
 		Client cli1 = new Client("Leblanc", "Leonard", ad01, null);
+		Client cli2 = new Client("Nurte", "Nathan", ad02, null);
+		Client cli3 = new Client("Marron", "Claire", ad03, null);
 		
-	
+		Location loc1 = new Location(cli1,gyro3);
+		Location loc2 = new Location(cli2, velo3);
+		Location loc3 = new Location(cli3, velo3);
+		mag1.ajouterLocation(loc1);
+		mag1.ajouterLocation(loc2);
+		mag1.ajouterLocation(loc3);
+		mag1.getListeLocation().get(1).isEstRegle();
+		mag1.getListeLocation().get(1).terminerLocation();
 		int choix = -1;
+		
 		while(choix != 0) {
 		System.out.println("⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜Bienvenue sur Locaciel⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜⚜");
 		System.out.println("Faites votre choix :");
 		System.out.println("1 - Chercher un article");
 		System.out.println("2 - Créer un article");
 		System.out.println("3 - Supprimer un article");
+		System.out.println("4 - Locations en cours");
 		
 		choix = sc.nextInt();
-		switch(choix) {
-		case 1:{
-			rechercheArticle(mag1);
-			break;
+			switch(choix) {
+				case 1:{
+					mag1.rechercheVehicule();
+					break;
+				}
+				case 2:{
+					mag1.creerArticle();
+					break;
+				}
+				case 3:{
+					supprimerArticle(mag1);
+					break;
+				}
+				case 4:{
+					mag1.consulterLocationEnCours();
+					break;
+				}
+			}
 		}
-		case 2:{
-			creerArticle(mag1);
-		}
-		case 3:{
-			supprimerArticle(mag1);
-		}
-		}
-		}
-	}
+}
 	
-	private static void creerArticle(Magasin mag1) {
-		int choix = -1;
-		
-		System.out.println("Voulez vous entrer :");
-		System.out.println("1 - Velo");
-		System.out.println("2 - Gyropode");
-		System.out.println("3 - Gyroroue");
-		choix = sc.nextInt();
-		System.out.println("Veuillez entrer la marque :");
-		sc.nextLine();
-		String marque = sc.nextLine();
-		System.out.println("Veuillez entrer le modèle :");
-		String modele = sc.nextLine();
-		LocalDate date = LocalDate.now();
-		switch(choix) {
-		case 1 :{
-			System.out.println("Veuillez entrer le nombre de vitesses");
-			int vitesse = sc.nextInt();
-			Velo velo = new Velo(marque,modele,date,vitesse);
-			mag1.ajouterVehicule(velo);
-			break;
-		}
-		case 2 :{
-			System.out.println("Veuillez entrer l'autonomie");
-			int autonomie = sc.nextInt();
-			System.out.println("Veuillez entrer la taille minimum");
-			double taillemin = sc.nextDouble();
-			Gyropode gyropode = new Gyropode(marque,modele,date,autonomie,taillemin);
-			mag1.ajouterVehicule(gyropode);
-			break;
-		}
-		case 3 :{
-			System.out.println("Veuillez entrer l'autonomie");
-			int autonomie = sc.nextInt();
-			Gyroroue gyroroue = new Gyroroue(marque,modele,date,autonomie);
-			mag1.ajouterVehicule(gyroroue);
-			break;
-		}default :{
-			System.out.println("Veuillez entrer une valeur correcte");
-		}
-		}
-		
-	}
-
-	public static void rechercheArticle(Magasin mag1) {
-		String recherche = "";
-		System.out.println("Veuillez rentrer le nom du modèle à rechercher :");
-		sc.nextLine();
-		recherche = sc.nextLine();
-		List<Vehicule> vRecherche = mag1.chercherVehicule(recherche);
-		afficherVehicule(vRecherche);
-	}
 	
-	public static void afficherVehicule(List<Vehicule> listeVehiculeTrouve) {
 
-		System.out.println("🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲Liste des Articles🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲🚲");
-		for(int i = 0; i < listeVehiculeTrouve.size(); i++) {
-			System.out.println(i);
-			System.out.println(listeVehiculeTrouve.get(i).toString());
-		}
-	}
+
+	
+
 	
 	public static void supprimerArticle(Magasin mag1) {
 		int choix = -1;
@@ -137,4 +100,6 @@ public class Locaciel {
 		System.out.println("Article supprimé");
 		
 	}
+	
+
 }
